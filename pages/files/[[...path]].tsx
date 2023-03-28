ef54b7072c26d50e8ed4d1c64c97fe97e8e8e330
deafnv <rokiniri@yahoo.in>
2023-03-28 10:33:40 +0800

    feat: custom context menu

--- pages/files/[[...path]].tsx
+++ pages/files/[[...path]].tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import axios from 'axios'
 import prettyBytes from 'pretty-bytes'
 import Link from 'next/link'
-import { useCallback, useEffect, useRef, useState } from 'react'
+import { useCallback, useEffect, useRef, useState, RefObject, Dispatch, SetStateAction } from 'react'
 import { useDropzone } from 'react-dropzone'
 import { useRouter } from 'next/router'
 import { FileServerFile } from '@/lib/types'
@@ -19,7 +19,10 @@ import ClosedCaptionIcon from '@mui/icons-material/ClosedCaption'
 
 export default function Files() {
   const paramsRef = useRef<string[]>([])
+  const fileListRef = useRef<HTMLDivElement>(null)
+  const contextMenuRef = useRef<HTMLMenuElement>(null)
 
+  const [contextMenu, setContextMenu] = useState<FileServerFile | 'directory' | null>(null)
   const [fileArr, setFileArr] = useState<FileServerFile[] | string | null>(null)
 
   const router = useRouter()
@@ -39,6 +42,38 @@ export default function Files() {
     getData()
   }, [router.asPath])
 
+  useEffect(() => {
+    const customContextMenu = (e: MouseEvent) => {
+      const target = e.target as HTMLElement
+      if (contextMenuRef.current?.contains(target)) {
+        e.preventDefault()
+        return
+      }
+      if (!fileListRef.current || !fileListRef.current.contains(target)) return
+
+      e.preventDefault()
+      if (target == fileListRef.current) {
+        setContextMenu('directory')
+      }
+      if (!contextMenuRef.current) return
+      contextMenuRef.current.style.top = `${e.pageY}px`
+      contextMenuRef.current.style.left = `${e.pageX}px`
+    }
+    
+    const exitContextMenu = (e: MouseEvent) => {
+      const target = e.target as HTMLElement
+      if (!contextMenuRef.current) return
+      if (!contextMenuRef.current?.contains(target)) {
+        setContextMenu(null)
+      }
+    }
+
+    document.addEventListener("contextmenu", customContextMenu)
+    document.addEventListener("click", exitContextMenu)
+    
+    return () => document.removeEventListener("contextmenu", customContextMenu)
+  }, [])
+
   const onDrop = useCallback((acceptedFiles: File[]) => {
     const formData = new FormData()
     acceptedFiles.forEach((file) => {
@@ -54,10 +89,12 @@ export default function Files() {
         <meta name="description" content="File Server" />
       </Head>
       <main className="flex items-center justify-center">
-        <section className='h-[calc(100dvh-60px)] w-[20%] bg-gray-500'>
-          
+        <section className='flex flex-col items-center h-[calc(100dvh-60px)] w-[15%] bg-gray-700'>
+          <div className='mt-24 p-2 cursor-pointer rounded-lg hover:bg-black'>
+            <span className='text-lg'>Storage Space</span>
+          </div>
         </section>
-        <section className='p-12 h-[calc(100dvh-60px)] w-[80%] bg-slate-600'>
+        <section className='p-12 h-[calc(100dvh-60px)] w-[85%] bg-slate-600'>
           <span className='text-xl'>
             <Link 
               href={''}
@@ -78,7 +115,7 @@ export default function Files() {
               </>
             ))}
           </span>
-          <FileList fileArr={fileArr} />
+          <FileList fileArr={fileArr} fileListRef={fileListRef} contextMenu={contextMenu} setContextMenu={setContextMenu} />
           {/* <div {...getRootProps()}>
             <input {...getInputProps()} />
             {
@@ -88,12 +125,57 @@ export default function Files() {
             }
           </div> */}
         </section>
+        {contextMenu && <ContextMenu contextMenuRef={contextMenuRef} contextMenu={contextMenu} />}
       </main>
     </>
   )
 }
 
-function FileList({ fileArr }: { fileArr: FileServerFile[] | string | null }) {
+function ContextMenu({ contextMenuRef, contextMenu }: { contextMenuRef: RefObject<HTMLMenuElement>; contextMenu:  FileServerFile | 'directory' | null; }) {
+  if (!contextMenu) return null //? Return context menu for clicking on empty space here
+
+  if (contextMenu == 'directory') {
+    return (
+      <menu
+        ref={contextMenuRef}
+        className="absolute min-w-[12rem] z-10 p-3 shadow-lg shadow-gray-700 bg-slate-200 text-black text-lg rounded-md border-black border-solid border-2 context-menu"
+      >
+        Show New Folder and stuff
+      </menu>
+    )
+  }
+
+  return (
+    <menu
+      ref={contextMenuRef}
+      className="absolute min-w-[12rem] z-10 p-3 shadow-lg shadow-gray-700 bg-slate-200 text-black text-lg rounded-md border-black border-solid border-2 context-menu"
+    >
+      <li className="flex justify-center h-8 rounded-sm hover:bg-slate-500">
+        <button onClick={() => {}} className="w-full">
+          Open
+        </button>
+      </li>
+      <li className="flex justify-center h-8 rounded-sm hover:bg-slate-500">
+        <button onClick={() => {}} className="w-full">
+          Copy Link
+        </button>
+      </li>
+      <hr className="my-2 border-gray-500 border-t-[1px]" />
+      <li className="flex justify-center h-8 rounded-sm hover:bg-slate-500">
+        <button onClick={() => {}} className="w-full">
+          Delete
+        </button>
+      </li>
+      <li className="flex justify-center h-8 rounded-sm hover:bg-slate-500">
+        <button onClick={() => {}} className="w-full">
+          Rename
+        </button>
+      </li>
+    </menu>
+  )
+}
+
+function FileList({ fileArr, fileListRef, contextMenu, setContextMenu }: { fileArr: FileServerFile[] | string | null; fileListRef: RefObject<HTMLDivElement>; contextMenu:  FileServerFile | 'directory' | null; setContextMenu: Dispatch<SetStateAction<FileServerFile | 'directory' | null>>; }) {
   if (fileArr == null || fileArr == 'Error loading data from server') {
     return (
       <div className='flex flex-col m-4 p-2 pt-0 h-full w-full bg-black rounded-lg overflow-auto'>
@@ -126,7 +208,10 @@ function FileList({ fileArr }: { fileArr: FileServerFile[] | string | null }) {
   }
 
   return (
-    <div className='flex flex-col m-4 p-2 pt-0 h-full w-full bg-black rounded-lg overflow-auto'>
+    <div
+      ref={fileListRef}
+      className='flex flex-col m-4 p-2 pt-0 h-full w-full bg-black rounded-lg overflow-auto'
+    >
       <div className='sticky top-0 flex text-lg border-b-[1px] bg-black'>
         <span className='p-3 min-w-[2.5rem] max-w-[2.5rem]'></span>
         <span className='p-3 flex-grow'>Name</span>
@@ -138,7 +223,8 @@ function FileList({ fileArr }: { fileArr: FileServerFile[] | string | null }) {
           <Link 
             key={index}
             href={file.isDirectory ? `/files${file.path}` : `${process.env.NEXT_PUBLIC_FILE_SERVER_URL}/retrieve${file.path}`}
-            className='flex text-lg rounded-md cursor-pointer hover:bg-gray-500'
+            onContextMenu={() => setContextMenu(file)}
+            className={`flex text-lg rounded-md cursor-pointer ${contextMenu == file ? 'bg-gray-500' : ''} hover:bg-gray-500`}
           >
             <span className='p-3 min-w-[2.5rem] max-w-[2.5rem]'>{getIcon(file)}</span>
             <span className='p-3 flex-grow'>{file.name}</span>
